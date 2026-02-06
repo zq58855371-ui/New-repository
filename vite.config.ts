5cd337cd22cc267ce58a34ebae3a9f59204b934e
zq58855371-ui <zq58855371@gmail.com>
2026-02-07 00:20:32 +0800

    update vite base and homepage

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/New-repository/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
